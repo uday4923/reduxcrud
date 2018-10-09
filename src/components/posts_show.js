75d2c5725beb0529c26410f2f81d5ab0bc251417
udaykumar4923 <udaykumar@qainfotech.com>
2018-10-09 23:34:11 +0530

    initial commit

---
+++ src/components/posts_show.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PostsShow extends Component{
+  render(){
+    return(
+      <div>
+        Posts Show
+      </div>
+    );
+  };
+}
+
+
+
+
+export default PostsShow;
